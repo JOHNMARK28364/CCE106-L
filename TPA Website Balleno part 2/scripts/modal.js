10f5db94fc0db49939f28283b3d2bd5e32784c46
JOHNMARK28364 <johnmarkramos733@gmail.com>
2024-10-15 15:12:41 +0800

    modal.js

--- TPA Website Balleno part 2/scripts/modal.js
+++ TPA Website Balleno part 2/scripts/modal.js
@@ -6,7 +6,6 @@ function closeFormModal() {
   document.getElementById("formModal").style.display = "none";
 }
 
-// Close modal when clicking outside of it
 window.onclick = function(event) {
   if (event.target === document.getElementById("formModal")) {
     closeFormModal();
